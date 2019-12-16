db56237569ba8b5efebb95f87e1b0e8ec711db30
poojachirp <poojasrinath@gmail.com>
2019-12-15 22:02:53 -0500

    Added implementation that will write incoming greetings to a file

--- src/main/java/com/twilio/formhandling/GreetingController.java
+++ src/main/java/com/twilio/formhandling/GreetingController.java
@@ -1,13 +1,21 @@
 package com.twilio.formhandling;
 
+import com.twilio.formhandling.model.Greeting;
+import com.twilio.formhandling.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
+
 @Controller
 public class GreetingController {
+
+    @Autowired
+    GreetingService service;
 
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
@@ -17,6 +25,12 @@ public class GreetingController {
 
     @PostMapping("/greeting")
     public String greetingSubmit(@ModelAttribute Greeting greeting) {
+
+        try {
+            service.save(greeting);
+        } catch (IOException e) {
+          return  "error";
+        }
         return "result";
     }
 
--- src/main/java/com/twilio/formhandling/model/Greeting.java
+++ src/main/java/com/twilio/formhandling/model/Greeting.java
@@ -1,4 +1,4 @@
-package com.twilio.formhandling;
+package com.twilio.formhandling.model;
 
 public class Greeting {
 
@@ -21,4 +21,11 @@ public class Greeting {
         this.content = content;
     }
 
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
